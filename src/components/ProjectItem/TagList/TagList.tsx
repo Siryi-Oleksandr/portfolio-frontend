609b0f2de0a561c3235b718928e14c14c551078b
Oleksandr Siryi <siryyoleksandr@gmail.com>
2023-07-23 09:27:27 +0300

    fix tag list

--- src/components/ProjectItem/TagList/TagList.tsx
+++ src/components/ProjectItem/TagList/TagList.tsx
@@ -1,18 +1,18 @@
-import React, {FC} from 'react'
-import { List } from './TagList.styled'
-import TagItem from '../TagItem/TagItem'
+import React, { FC } from 'react';
+import { List } from './TagList.styled';
+import TagItem from '../TagItem/TagItem';
 
 const TagList: FC = () => {
   return (
     <List>
-        <TagItem tagName="HTML" />
-        <TagItem tagName="CSS" />
-        <TagItem tagName="JavaScript" />
-        <TagItem tagName="React" />
-        <TagItem tagName="Redux" />
-          <TagItem tagName="Next.js" />
+      <TagItem tagName="HTML" />
+      <TagItem tagName="CSS" />
+      <TagItem tagName="JavaScript" />
+      <TagItem tagName="React" />
+      <TagItem tagName="Redux" />
+      <TagItem tagName="Next.js" />
     </List>
-  )
-}
+  );
+};
 
-export default TagList
+export default TagList;
