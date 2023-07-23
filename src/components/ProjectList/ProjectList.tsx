267316f84d5f2c5c40a9b1ebedb91ccdf4cae772
Oleksandr Siryi <siryyoleksandr@gmail.com>
2023-07-23 22:53:26 +0300

    fix eslint

--- src/components/ProjectList/ProjectList.tsx
+++ src/components/ProjectList/ProjectList.tsx
@@ -1,16 +1,17 @@
-import React, { FC, useState } from "react";
-import ProjectItem from "components/ProjectItem/ProjectItem";
-import { List } from "./ProjectList.styled";
-import projectsDb from "bd/projects.json";
-import { IProject } from "types/projectTypes";
+import React, { FC, useState } from 'react';
+import ProjectItem from 'components/ProjectItem/ProjectItem';
+import { List } from './ProjectList.styled';
+import projectsDb from 'bd/projects.json';
+import { IProject } from 'types/projectTypes';
 
 const ProjectList: FC = () => {
-  const [projects, setProjects] = useState<Array<IProject>>(projectsDb || []);
+  const [projects] = useState<Array<IProject>>(projectsDb || []);
 
   return (
     <List>
-      {projects.map(project => <ProjectItem project={project} />)}
-    
+      {projects.map(project => (
+        <ProjectItem project={project} />
+      ))}
     </List>
   );
 };
