a92cc1fc2db9d94246ddb4c2c19070b4f36e4363
Malakhow-Alexandr <chuk3pc@gmail.com>
2023-07-24 15:46:12 +0300

    add dependencies

---
+++ src/components/RegisterForm/RegisterForm.styled.tsx
@@ -0,0 +1,14 @@
+import * as Yup from 'yup';
+
+const regExp =
+  /^([A-z0-9_-]+\.)*[A-z0-9_-]+@[A-z0-9_-]+(\.[A-z0-9_-]+)*\.[A-z]{2,6}$/;
+
+const FormSchema = Yup.object().shape({
+  name: Yup.string().min(2).max(35).required('Required'),
+  email: Yup.string()
+    .matches(regExp, 'Invalid email address')
+    .required('Required'),
+  password: Yup.string()
+    .min(6, 'Password must be 6 characters long')
+    .required('Required'),
+});
