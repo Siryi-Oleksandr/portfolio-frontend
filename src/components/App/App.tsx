e8a48e332bfac8bdac43b5b105a7ad3f2615aef7
KobzarAnatolii <TohaPirat@gmail.com>
2023-07-24 13:41:53 +0300

    add layout

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,32 +1,37 @@
-import React, { FC } from "react";
-import { Route, Routes } from "react-router-dom";
-import { Navigation, NotFoundPage } from "components";
-import {  HomePage,
+import React, { FC } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { Layout, NotFoundPage } from 'components';
+import {
+  HomePage,
   PortfolioPage,
   CabinetPage,
   SearchPage,
   AboutPage,
-  ContactsPage, ProjectDetails} from "pages";
-import GlobalStyles from "GlobalStyle";
-import { Container } from "./App.styled";
+  ContactsPage,
+  ProjectDetails,
+} from 'pages';
+import GlobalStyles from 'GlobalStyle';
 
 const App: FC = () => {
   return (
-    <Container>
+    <>
       <GlobalStyles />
       <Routes>
-        <Route path="/" element={<Navigation />}>
+        <Route path="/" element={<Layout />}>
           <Route index element={<HomePage />} />
-          <Route path="/search" element={<SearchPage/>} />
-          <Route path="/cabinet" element={<CabinetPage/>} />
-          <Route path="/portfolio" element={<PortfolioPage/>} />
-          <Route path="/projectDetails/:projectId" element={<ProjectDetails/>} />
-          <Route path="/about" element={<AboutPage/>} />
-          <Route path="/contacts" element={<ContactsPage/>} />
+          <Route path="/search" element={<SearchPage />} />
+          <Route path="/cabinet" element={<CabinetPage />} />
+          <Route path="/portfolio" element={<PortfolioPage />} />
+          <Route
+            path="/projectDetails/:projectId"
+            element={<ProjectDetails />}
+          />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/contacts" element={<ContactsPage />} />
         </Route>
-         <Route path="*" element={<NotFoundPage />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
-    </Container>
+    </>
   );
 };
 
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,13 +1,7 @@
 import React, { FC } from 'react';
-import { Footer } from 'components';
 
 const HomePage: FC = () => {
-  return (
-    <>
-      <h1>HomePage</h1>
-      <Footer />
-    </>
-  );
+  return <h1>HomePage</h1>;
 };
 
 export default HomePage;
