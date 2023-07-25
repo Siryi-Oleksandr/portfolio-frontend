6ea342046166ca74ca0ce75baf6c21caa595ce90
Oleksandr Siryi <siryyoleksandr@gmail.com>
2023-07-25 20:22:48 +0300

    add container

--- src/pages/PortfolioPage/PortfolioPage.tsx
+++ src/pages/PortfolioPage/PortfolioPage.tsx
@@ -3,10 +3,10 @@ import { ProjectList, MainBtn, Container } from 'components';
 
 const Portfolio: FC = () => {
   return (
-    <>
+    <Container>
       <MainBtn text="Let's Talk" />
       <ProjectList />
-    </>
+    </Container>
   );
 };
 
