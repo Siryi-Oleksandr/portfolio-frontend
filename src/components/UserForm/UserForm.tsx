ad029793bc2876f68674366894be1be5b53d4ef6
KobzarAnatolii <TohaPirat@gmail.com>
2023-07-27 20:32:04 +0300

    fix user form

--- src/components/UserForm/UserForm.tsx
+++ src/components/UserForm/UserForm.tsx
@@ -1,8 +1,8 @@
 import { FC } from 'react';
 import { selectUser } from 'redux/auth/authSelectors';
-import { useAppSelector } from 'redux/reduxHooks';
+import { useAppDispatch, useAppSelector } from 'redux/reduxHooks';
 import { IUser } from '../../types/userTypes';
-import { Formik, ErrorMessage } from 'formik';
+import { Formik, ErrorMessage, FormikHelpers } from 'formik';
 import {
   InputsContainer,
   Label,
@@ -12,30 +12,38 @@ import {
   SubmitBtn,
 } from 'components/RegisterForm/RegisterForm.styled';
 import { StyledUserForm } from './UserForm.styled';
+import { IUpdateUser } from 'redux/reduxTypes';
+import { updateUser } from 'redux/auth/operations';
 
 const UserForm: FC = () => {
+  const dispatch = useAppDispatch();
   const user: IUser = useAppSelector(selectUser);
+
+  const initialValues: IUpdateUser = {
+    name: user.name,
+    surname: user.surname || '',
+    email: user.email,
+    profession: user.profession || '',
+    experience: String(user.experience) || '',
+    phone: user.phone || '',
+    telegram: user.telegram || '',
+    linkedinURL: user.linkedinURL || '',
+    gitHubURL: user.gitHubURL || '',
+    summary: user.summary || '',
+  };
+
+  const handleSubmit = (
+    values: IUpdateUser,
+    actions: FormikHelpers<IUpdateUser>
+  ) => {
+    console.log(values);
+    actions.resetForm();
+    dispatch(updateUser(values));
+  };
 
   return (
     <>
-      <Formik
-        initialValues={{
-          name: user.name,
-          surname: user.surname,
-          email: user.email,
-          profession: user.profession,
-          experience: user.experience,
-          phone: user.phone,
-          telegram: user.telegram,
-          linkedinURL: user.linkedinURL,
-          gitHubURL: user.gitHubURL,
-          summary: user.summary,
-        }}
-        onSubmit={async values => {
-          await new Promise(resolve => setTimeout(resolve, 500));
-          alert(JSON.stringify(values, null, 2));
-        }}
-      >
+      <Formik initialValues={initialValues} onSubmit={handleSubmit}>
         <StyledUserForm>
           <InputsContainer>
             <Label>
@@ -58,11 +66,11 @@ const UserForm: FC = () => {
               <StyledLabel>Profession</StyledLabel>
               <ErrorMessage component={StyledErrorMessage} name="profession" />
             </Label>
-            <Label>
+            {/* <Label>
               <StyledField type="number" min="0" step="0.5" name="experience" />
               <StyledLabel>Experience</StyledLabel>
               <ErrorMessage component={StyledErrorMessage} name="experience" />
-            </Label>
+            </Label> */}
             <Label>
               <StyledField type="text" name="phone" />
               <StyledLabel>Phone</StyledLabel>
