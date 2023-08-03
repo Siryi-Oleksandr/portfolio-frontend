4cbc98d51579a9d247bafd1297cce40c099779c4
KovalMaxim1989 <110034072+KovalMaxim1989@users.noreply.github.com>
2023-08-03 17:43:16 +0300

    some-info-styles
    some-info-styles

--- src/components/Navigation/Logo/Logo.styled.tsx
+++ src/components/Navigation/Logo/Logo.styled.tsx
@@ -1,24 +1,23 @@
 import styled from '@emotion/styled';
 import { Link } from 'react-router-dom';
-import { theme } from "theme";
+import { theme } from 'theme';
 
 export const LogoIcon = styled(Link)`
-    margin-right: 20px;
-    transition: ${theme.transition.hover};
-    border-radius: 37% 5% 5% 37%;
-    @media screen and (max-width: ${theme.breakpoints.mobileMax}) {
+  margin-right: 8px;
+  transition: ${theme.transition.hover};
+  border-radius: 37% 5% 5% 37%;
+  @media screen and (max-width: ${theme.breakpoints.mobileMax}) {
     margin-right: 8px;
   }
-    &:hover {
-     transform: scale(1.05);
-     box-shadow: 0 0 12px 4px ${theme.colors.accentColor} ;
-      }
+  &:hover {
+    transform: scale(1.05);
+    box-shadow: 0 0 12px 4px ${theme.colors.accentColor};
+  }
 `;
 export const Img = styled.img`
-height: 50px;
-opacity: 0.7;
-&:hover {
+  height: 50px;
+  opacity: 0.7;
+  &:hover {
     opacity: 1;
-    }
+  }
 `;
-
