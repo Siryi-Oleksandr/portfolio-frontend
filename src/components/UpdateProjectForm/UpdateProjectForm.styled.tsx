d4616e08d32592fb47c7305a2692d9120cb3fc40
KobzarAnatolii <TohaPirat@gmail.com>
2023-08-07 13:31:36 +0300

    add markup UpdateProjectForm

--- src/components/UpdateProjectForm/UpdateProjectForm.styled.tsx
+++ src/components/UpdateProjectForm/UpdateProjectForm.styled.tsx
@@ -1,1 +1,126 @@
-export {};
+import styled from '@emotion/styled';
+import { theme } from 'theme';
+import { Field, Form } from 'formik';
+import { BiSolidImageAdd } from 'react-icons/bi';
+
+export const StyledAddProjectForm = styled(Form)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 36px;
+  margin-top: 50px;
+  margin-bottom: 50px;
+
+  @media screen and (min-width: ${theme.breakpoints.tablet}) {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+  }
+`;
+
+export const StyledProdjecField = styled(Field)`
+  display: block;
+  width: 300px;
+  min-width: 100%;
+  height: 36px;
+  padding: 8px 5px;
+  border: 2px solid #ccc;
+  background-color: ${theme.colors.textarea};
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 500;
+  font-family: ${theme.fonts.heading};
+  color: ${theme.colors.blackTitle};
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
+  transition: all 250ms ease-in-out;
+
+  @media screen and (min-width: ${theme.breakpoints.tablet}) {
+    width: 100%;
+    height: 48px;
+    padding: 14px 5px;
+    font-size: 16px;
+    font-weight: 600;
+  }
+
+  &:focus {
+    outline: none;
+    border-color: ${theme.colors.accentColor};
+    box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.2);
+  }
+  &:focus ~ span {
+    color: ${theme.colors.accentColor};
+  }
+`;
+
+export const StyledLabel = styled.span`
+  position: absolute;
+  top: -20px;
+  left: 0;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 600;
+  font-family: ${theme.fonts.heading};
+  color: ${theme.colors.primary_text_switch};
+  transition: all 250ms ease-in-out;
+`;
+
+export const Label = styled.label`
+  position: relative;
+`;
+
+export const LabelTextArea = styled.label`
+  position: relative;
+
+  @media screen and (min-width: ${theme.breakpoints.tablet}) {
+    grid-column: 1 / 3;
+    width: 100%;
+  }
+`;
+
+export const ImageWrap = styled.div`
+  width: 300px;
+  height: 200px;
+  background-color: white;
+  border: 1px solid #999;
+  border-radius: 4px;
+  overflow: hidden;
+  box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.2);
+
+  @media screen and (min-width: ${theme.breakpoints.tablet}) {
+    width: 237px;
+    height: 170px;
+  }
+
+  @media screen and (min-width: ${theme.breakpoints.desktop}) {
+    width: 376px;
+    height: 245px;
+  }
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+`;
+
+export const AddImgIcon = styled(BiSolidImageAdd)`
+  width: 35px;
+  height: 35px;
+  font-size: 12px;
+  cursor: pointer;
+  color: ${theme.colors.accentColor};
+`;
+
+export const ImagesWrap = styled.div`
+  grid-column: 1 / 3;
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+
+  @media screen and (min-width: ${theme.breakpoints.tablet}) {
+    flex-direction: row;
+  }
+
+  @media screen and (min-width: ${theme.breakpoints.desktop}) {
+    gap: 16px;
+  }
+`;
--- src/components/UpdateProjectForm/UpdateProjectForm.tsx
+++ src/components/UpdateProjectForm/UpdateProjectForm.tsx
@@ -1,1 +1,209 @@
-export {};
+import { ErrorMessage, Formik, FormikHelpers } from 'formik';
+import React, { FC, useState } from 'react';
+import {
+  Label,
+  LabelTextArea,
+  StyledAddProjectForm,
+  StyledProdjecField,
+  StyledLabel,
+  ImageWrap,
+  AddImgIcon,
+  ImagesWrap,
+} from './UpdateProjectForm.styled';
+import { SubmitBtn } from 'components/UserForm/UserForm.styled';
+import Container from 'components/Container/Container';
+import { ICreateUpdateProject } from 'redux/reduxTypes';
+import { FormAddProjectUpdateSchema } from 'services/yupSchemas';
+import { StyledErrorMessage } from 'components/RegisterForm/RegisterForm.styled';
+import { useAppDispatch } from 'redux/reduxHooks';
+import { createProject } from 'redux/project/operations';
+import placeholder from '../../images/placeholder-image.jpg';
+import { useNavigate } from 'react-router-dom';
+import { handleFormikImageUpload } from 'services';
+
+const UpdateProjectForm: FC = () => {
+  const dispatch = useAppDispatch();
+  const [projectImg1, setProjectImg1] = useState<string>(placeholder);
+  const [projectImg2, setProjectImg2] = useState<string>(placeholder);
+  const [projectImg3, setProjectImg3] = useState<string>(placeholder);
+  const navigate = useNavigate();
+
+  const initialValues: ICreateUpdateProject = {
+    projectTitle: '',
+    projectSubTitle: '',
+    projectLink: '',
+    codeLink: '',
+    image1: undefined,
+    image2: undefined,
+    image3: undefined,
+    aboutProject: '',
+    technicalStack: '',
+  };
+
+  const handleSubmit = (
+    values: ICreateUpdateProject,
+    actions: FormikHelpers<ICreateUpdateProject>
+  ) => {
+    actions.resetForm();
+    dispatch(createProject(values));
+    navigate('/cabinet');
+  };
+
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={FormAddProjectUpdateSchema}
+      onSubmit={handleSubmit}
+    >
+      {props => (
+        <Container>
+          <StyledAddProjectForm>
+            <Label>
+              <StyledProdjecField
+                type="text"
+                name="projectTitle"
+                autoComplete="off"
+              />
+              <StyledLabel>Project Title</StyledLabel>
+              <ErrorMessage
+                component={StyledErrorMessage}
+                name="projectTitle"
+              />
+            </Label>
+            <Label>
+              <StyledProdjecField
+                type="text"
+                name="technicalStack"
+                autoComplete="off"
+                placeholder="TypeScript, React, Redux"
+              />
+              <StyledLabel>Technical Stack</StyledLabel>
+              <ErrorMessage
+                component={StyledErrorMessage}
+                name="technicalStack"
+              />
+            </Label>
+            <LabelTextArea>
+              <StyledProdjecField
+                type="text"
+                name="projectSubTitle"
+                className="full-width"
+                component="textarea"
+                autoComplete="off"
+                onChange={props.handleChange}
+              />
+              <StyledLabel>Project SubTitle</StyledLabel>
+              <ErrorMessage
+                component={StyledErrorMessage}
+                name="projectSubTitle"
+              />
+            </LabelTextArea>
+            <Label>
+              <StyledProdjecField
+                type="text"
+                name="projectLink"
+                autoComplete="off"
+                placeholder="Set link to project live page"
+              />
+              <StyledLabel>Project Link</StyledLabel>
+              <ErrorMessage component={StyledErrorMessage} name="projectLink" />
+            </Label>
+            <Label>
+              <StyledProdjecField
+                type="text"
+                name="codeLink"
+                autoComplete="off"
+              />
+              <StyledLabel>Code Link</StyledLabel>
+              <ErrorMessage component={StyledErrorMessage} name="codeLink" />
+            </Label>
+            <LabelTextArea>
+              <StyledProdjecField
+                type="text"
+                name="aboutProject"
+                className="full-width"
+                component="textarea"
+                autoComplete="off"
+                onChange={props.handleChange}
+                style={{ height: '120px' }}
+              />
+              <StyledLabel>About Project</StyledLabel>
+              <ErrorMessage
+                component={StyledErrorMessage}
+                name="aboutProject"
+              />
+            </LabelTextArea>
+            <ImagesWrap>
+              <Label>
+                <input
+                  type="file"
+                  multiple
+                  name="image1"
+                  onChange={event =>
+                    handleFormikImageUpload(
+                      event,
+                      props,
+                      'image1',
+                      setProjectImg1
+                    )
+                  }
+                  style={{ display: 'none' }}
+                />
+                <AddImgIcon />
+                <StyledLabel>Project Images</StyledLabel>
+                <ImageWrap>
+                  <img src={projectImg1} alt="project image1" />
+                </ImageWrap>
+              </Label>
+              <Label>
+                <input
+                  type="file"
+                  multiple
+                  name="image2"
+                  onChange={event =>
+                    handleFormikImageUpload(
+                      event,
+                      props,
+                      'image2',
+                      setProjectImg2
+                    )
+                  }
+                  style={{ display: 'none' }}
+                />
+                <AddImgIcon />
+                <ImageWrap>
+                  <img src={projectImg2} alt="project image2" />
+                </ImageWrap>
+              </Label>
+              <Label>
+                <input
+                  type="file"
+                  multiple
+                  name="image3"
+                  onChange={event =>
+                    handleFormikImageUpload(
+                      event,
+                      props,
+                      'image3',
+                      setProjectImg3
+                    )
+                  }
+                  style={{ display: 'none' }}
+                />
+                <AddImgIcon />
+                <ImageWrap>
+                  <img src={projectImg3} alt="project image3" />
+                </ImageWrap>
+              </Label>
+            </ImagesWrap>
+            <SubmitBtn type="submit" style={{ gridColumn: '1 / 3' }}>
+              Add new project
+            </SubmitBtn>
+          </StyledAddProjectForm>
+        </Container>
+      )}
+    </Formik>
+  );
+};
+
+export default UpdateProjectForm;
