c348906cd3360e174ddb3916581bca297560ee31
Oleksandr Siryi <siryyoleksandr@gmail.com>
2023-08-10 10:56:54 +0300

    change Logo

--- src/components/Navigation/Logo/Logo.styled.tsx
+++ src/components/Navigation/Logo/Logo.styled.tsx
@@ -5,19 +5,23 @@ import { theme } from 'theme';
 export const LogoIcon = styled(Link)`
   margin-right: 8px;
   transition: ${theme.transition.hover};
-  border-radius: 37% 5% 5% 37%;
+  border-radius: 50%;
   @media screen and (max-width: ${theme.breakpoints.mobileMax}) {
     margin-right: 8px;
   }
   &:hover {
     transform: scale(1.05);
-    box-shadow: 0 0 12px 4px ${theme.colors.accentColor};
+    /* box-shadow: 0 0 12px 4px ${theme.colors.accentColor}; */
   }
 `;
 export const Img = styled.img`
-  height: 50px;
-  opacity: 0.7;
+  width: 40px;
+  opacity: 0.8;
   &:hover {
     opacity: 1;
+  }
+
+  @media screen and (min-width: ${theme.breakpoints.tablet}) {
+    width: 50px;
   }
 `;
