b2d6081699c14dbcfcb4143c8c2c73fa39b07ecd
Oleksandr FIlippov <filya170895@gmail.com>
2023-08-14 16:25:25 +0200

    update

--- src/components/NotFoundPage/NotFoundPage.styled.tsx
+++ src/components/NotFoundPage/NotFoundPage.styled.tsx
@@ -1,5 +1,94 @@
 import styled from '@emotion/styled';
+import { keyframes } from '@emotion/react';
+import { Link } from 'react-router-dom';
+import { theme } from 'theme';
 
-export const Input = styled.input`
- border: none;
+const rotateAnimation = keyframes`
+   0% {
+    transform: rotate(0deg);
+  }
+ 
+  100% {
+    transform: rotate(360deg);
+  }
+`;
+
+export const Wrapper = styled.div`
+  text-align: center;
+  padding-top: 150px;
+`;
+
+export const NumberLogoWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 7px;
+`;
+
+export const NumberText = styled.p`
+  font-family: ${theme.fonts.title};
+  font-size: 100px;
+
+  color: ${theme.colors.primary_text_switch};
+
+  @media screen and (min-width: ${theme.breakpoints.tablet}) {
+    font-size: 120px;
+  }
+`;
+
+export const Logo = styled.img`
+  width: 100px;
+  animation: ${rotateAnimation} 10s linear infinite;
+
+  @media screen and (min-width: ${theme.breakpoints.tablet}) {
+    width: 125px;
+  }
+`;
+
+export const NotFoundText = styled.p`
+  font-family: ${theme.fonts.title};
+  font-size: 30px;
+  color: ${theme.colors.primary_text_switch};
+
+  @media screen and (min-width: ${theme.breakpoints.tablet}) {
+    font-size: 35px;
+  }
+`;
+
+export const InfoWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+  margin-top: 40px;
+`;
+
+export const InfoText = styled.p`
+  line-height: 25px;
+  font-family: ${theme.fonts.text};
+  font-size: 18px;
+  margin-bottom: 40px;
+  color: ${theme.colors.primary_text_switch};
+
+  @media screen and (min-width: ${theme.breakpoints.tablet}) {
+    font-size: 25px;
+  }
+`;
+
+export const NavigateText = styled.p`
+  font-family: ${theme.fonts.text};
+  font-size: 18px;
+  color: ${theme.colors.primary_text_switch};
+
+  @media screen and (min-width: ${theme.breakpoints.tablet}) {
+    font-size: 25px;
+  }
+`;
+
+export const HomeLink = styled(Link)`
+  color: ${theme.colors.accentColor};
+
+  :hover,
+  :focus {
+    text-decoration: underline;
+  }
 `;
