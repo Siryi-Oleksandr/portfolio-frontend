27bb52eb1aeabd4210ba88aaefc96a89d65ef2d5
KovalMaxim1989 <110034072+KovalMaxim1989@users.noreply.github.com>
2023-08-22 11:51:57 +0300

    user-from-container
    add userform containet

--- src/components/Cabinet/UserFormModal/UserFormModal.tsx
+++ src/components/Cabinet/UserFormModal/UserFormModal.tsx
@@ -1,6 +1,7 @@
 import { Modal } from 'components/Modal/Modal';
 import React, { FC } from 'react';
 import UserForm from 'components/UserForm/UserForm';
+import Container from 'components/Container/Container';
 
 type ModalUserFormPorps = {
   onClose: any;
@@ -9,9 +10,9 @@ type ModalUserFormPorps = {
 export const UserFormModal: FC<ModalUserFormPorps> = ({ onClose }) => {
   return (
     <Modal onClose={onClose}>
-      <div>
+      <Container>
         <UserForm onClose={onClose} />
-      </div>
+      </Container>
     </Modal>
   );
 };
